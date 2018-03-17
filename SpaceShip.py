e0cee2d2dcbfe5c0362d794d1df9000ce991bc87
mwsfr <35932118+mwsfr@users.noreply.github.com>
2018-03-17 16:24:30 -0300

    Add files via upload

---
+++ SpaceShip.py
@@ -0,0 +1,23 @@
+class SpaceShip:
+	# name = ''
+	# fuel = ''
+	# hull = ''
+	# power = ''
+	# robots = ''
+	# missiles = ''
+	# battleMode = False
+	# scanner = ''
+	# enemy = ''
+	# public ArrayList <SpaceShipRoom> roomList = new ArrayList<SpaceShipRoom>()
+	# public ArrayList <Person> crew = new ArrayList<Person>()
+
+	def __init__(self,name='unknown',fuel='',hull='',power='',robots='',missiles='',battleMode='',scanner='',enemy=''):
+		self.name = name
+		self.fuel=fuel
+		self.hull=hull
+		self.power=power
+		self.robots=robots
+		self.missiles=missiles
+		self.battleMode=battleMode
+		self.scanner=scanner
+		self.enemy=enemy	
---
+++ custom_init.py
@@ -0,0 +1,4 @@
+import sys
+sys.path.insert(0,'C:\\Users\\marcosreis\\Documents\\M\\MobileApp_v1\\py')
+from SpaceShip import SpaceShip
+sp = SpaceShip('Falcon',10,20,30,40,50,True,'no','empty')
